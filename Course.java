9d19cc1f8d0a2ce8428423494aaff9311c853f0e
saifhali20 <shali@email.sc.edu>
2024-02-29 13:38:38 -0500

    updated Course with constructor

--- Course.java
+++ Course.java
@@ -3,13 +3,17 @@ import java.util.ArrayList;
 public class Course {
     private String courseID;
     private String courseName;
-    //hashmap?
+    // hashmap?
     private ArrayList<Course> preReqs;
     private char minGrade;
     private String department;
 
-    public Course(String courseID, String courseName, String requirements, char minimumGrade, String department) {
-        // Constructor implementation here
+    public Course(String courseID, String courseName, char minimumGrade, String department) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.minGrade = minimumGrade;
+        this.department = department;
+
     }
 
     // Getter and setter methods here
