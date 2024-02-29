523a7b415af1a4d4469084110cf24c1cd9472da1
saifhali20 <shali@email.sc.edu>
2024-02-29 13:51:37 -0500

    added getters setters and constructor here

--- Major.java
+++ Major.java
@@ -6,5 +6,42 @@ public class Major {
     private ArrayList<Course> coreCourses;
     private ArrayList<Course> electiveCourses;
 
-    // Constructor and methods
+    public Major(String majorName, int requiredCredits) {
+        this.majorName = majorName;
+        this.requiredCredits = requiredCredits;
+        this.coreCourses = new ArrayList<Course>();
+        this.electiveCourses = new ArrayList<Course>();
+    }
+
+    public String getMajorName() {
+        return majorName;
+    }
+
+    public int getRequiredCredits() {
+        return requiredCredits;
+    }
+
+    public ArrayList<Course> getCoreCourses() {
+        return coreCourses;
+    }
+
+    public ArrayList<Course> getElectiveCourses() {
+        return electiveCourses;
+    }
+
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+
+    public void setRequiredCredits(int requiredCredits) {
+        this.requiredCredits = requiredCredits;
+    }
+
+    public void setCoreCourses(ArrayList<Course> coreCourses) {
+        this.coreCourses = coreCourses;
+    }
+
+    public void setElectiveCourses(ArrayList<Course> electiveCourses) {
+        this.electiveCourses = electiveCourses;
+    }
 }
