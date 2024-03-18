4fa0f36d96f94d6e2c06b288fe8dd1c3a625f923
saifhali20 <shali@email.sc.edu>
2024-03-17 21:48:43 -0400

    lots of changes

--- ProgressTracker.java
+++ ProgressTracker.java
@@ -10,12 +10,13 @@ public class ProgressTracker {
     private Map<Semester, Double> semesterGPA;
     private Map<Course, Grade> grades;
 
-    public void viewCompletedCourses(String studentID){
-        Student student = StudentList.getUser(studentID); 
-        ArrayList<Course> courses = student.getCoursesTaken();
+    public void viewCompletedCourses(String studentID) {
+        Student student = UserList.getUser(studentID);
+        ArrayList<Grade> courses = student.getCoursesTaken();
         System.out.println("Completed courses:");
 
     }
+
     public boolean addCompletedCourse(Course course) {
         // Implementation
     }
