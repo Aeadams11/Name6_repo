0addd4b6c18b4b7061bd6347e23efe0d5bc40921
aeadams11 <aeadams11@gmail.com>
2024-03-19 14:23:36 -0400

    JSON Testing

---
+++ UserTester.java
@@ -0,0 +1,37 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserTester {
+    @BeforeClass
+	public void oneTimeSetup() {
+		
+	}
+	
+	@AfterClass
+	public void oneTimeTearDown() {
+		
+	}
+	
+	@BeforeEach
+	public void setup() {
+		//runs before each test
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		//runs after each test
+	}
+
+    @Test
+    public void testEmailFormat() {
+        
+    }
+}
