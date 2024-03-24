3e7a50fb34c5c518a45fbaaf065e8a92e2ab9e87
aeadams11 <aeadams11@gmail.com>
2024-03-24 17:27:24 -0400

    adding legal guardian stubs and comments, attack later

--- LegalGuardian.java
+++ LegalGuardian.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+
 public class LegalGuardian extends Student {
     public LegalGuardian(String userID, String firstName, String lastName, String email, String password,
-            boolean permission) {
-        super(userID, firstName, lastName, email, password, permission);
+            boolean permission, UserType type) {
+            super(userID, firstName, lastName, email, password, email, password, null, null, null, type, permission);
     }
 
     private String firstName;
     private String lastName;
     private boolean permission;
+    private String email; 
+    // ArrayList to hold student(s) they are legal guardian of
+    private ArrayList<Student> students; 
 
     public String getFirstName() {
         return this.firstName; 
@@ -16,8 +21,27 @@ public class LegalGuardian extends Student {
         return this.lastName;
     }
 
-    public boolean getHasPermission() {
+    public boolean setEmail(String email) {
+        //return true if email has @
+        //idk how else to check is email is valid for any email
+        if(email.contains("@")){
+            this.email = email;
+            return true; 
+        }
+        else   
+            return false; 
+    }
+    public void addStudent(Student student) {
         //return true if student has permission
-        return true;
+        //pass in a student, if that student has permissions set to true
+        //then add that student to their array list of students
+    }
+
+    public String viewStudentReport() {
+        return "Your student's report"; 
+    }
+
+    public String viewStudentProfile() {
+        return "Your student's profile"; 
     }
 }
