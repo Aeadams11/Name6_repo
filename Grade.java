5977df8a95dba1ecae8178b0cae11511a73ef759
Jmshealy1 <jmshealy@email.sc.edu>
2024-03-24 21:27:25 -0400

    renamed method in Grades

--- Grade.java
+++ Grade.java
@@ -1,5 +1,5 @@
 public class Grade {
-    private int score; 
+    private int score; // Numerical score
 
     public Grade(int score) {
         this.score = score;
@@ -9,19 +9,19 @@ public class Grade {
         return score;
     }
 
+    public double getGradeValue() {
+        if (score >= 90) return 4.0;
+        else if (score >= 80) return 3.0;
+        else if (score >= 70) return 2.0;
+        else if (score >= 60) return 1.0;
+        else return 0.0;
+    }
+
     public String toLetterGrade() {
         if (score >= 90) return "A";
         else if (score >= 80) return "B";
         else if (score >= 70) return "C";
         else if (score >= 60) return "D";
         else return "F";
-    }
-
-    public double toGPA() {
-        if (score >= 90) return 4.0;
-        else if (score >= 80) return 3.0;
-        else if (score >= 70) return 2.0;
-        else if (score >= 60) return 1.0;
-        else return 0.0;
     }
 }
