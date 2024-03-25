2fc913d63f0ac229d632fde3ee8df2f424a51565
Nazire Brown <nlb1@email.sc.edu>
2024-03-25 00:04:18 -0400

    Updated tester

--- ApplicationAreaTester.java
+++ ApplicationAreaTester.java
@@ -27,6 +27,27 @@ class ApplicationAreaTester {
 	
 	@AfterEach
 	public void tearDown() {
+    }
+    @Test
+    public void testAddRequiredCourse() {
+        Major major = new Major("Computer Science", null, null);
+        Course course = new Course("2e2ebf3f-c4d3-4dae-956b-d8afa7fca627", "CSCE", null, null, null, 0, null, null, null);
+        major.addRequiredCourse(course);
 
+        ArrayList<Course> requiredCourses = major.getRequiredCourses();
+        assertEquals(1, requiredCourses.size());
+        assertEquals(course, requiredCourses.get(0));
+    }
+    @Test
+    public void testAddOptionalCourse() {
+        Major major = new Major("Computer Science", null, null);
+        Course course = new Course("0ac7ca78-5ead-4438-ba85-3e0259eed7a8", "CSCE", "204", "Program Design and Development", null, 0, null, null, null);
+        major.addElectiveCourse(course);
+
+        ArrayList<Course> electiveCourses = major.getElectiveCourses();
+        assertEquals(1, electiveCourses.size());
+        assertEquals(course, electiveCourses.get(0));
     }
 }
+
+    
