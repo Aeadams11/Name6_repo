c6e0d4aaa93a0cff8539f9fe8710ee17c45ee587
Nazire Brown <nlb1@email.sc.edu>
2024-03-24 23:55:53 -0400

    Updated MajorTester

--- MajorTester.java
+++ MajorTester.java
@@ -36,7 +36,6 @@ class MajorTester {
         Major major = new Major("Computer Science", null, null);
         Course course = new Course("CS101", "Introduction to Computer Science", null, null, null, 0, null, null, null);
         major.addRequiredCourse(course);
-
         ArrayList<Course> requiredCourses = major.getRequiredCourses();
         assertEquals(1, requiredCourses.size());
         assertEquals(course, requiredCourses.get(0));
@@ -47,7 +46,6 @@ class MajorTester {
         Major major = new Major("Computer Science", null, null);
         Course course = new Course("0ac7ca78-5ead-4438-ba85-3e0259eed7a8", "CSCE", "204", "Program Design and Development", null, 0, null, null, null);
         major.addElectiveCourse(course);
-
         ArrayList<Course> electiveCourses = major.getElectiveCourses();
         assertEquals(1, electiveCourses.size());
         assertEquals(course, electiveCourses.get(0));
@@ -58,7 +56,6 @@ class MajorTester {
         Major major = new Major("Computer Science", null, null);
         Course course1 = new Course("3cca9c4e-a195-4759-a761-cfe09adb20b8", "CSCE", "190", "Computing in the Modern World", null, 0, null, null, null);
         major.addRequiredCourse(course1);
-
         ArrayList<Course> requiredCourses = major.getRequiredCourses();
         assertEquals(1, requiredCourses.size());
         assertEquals(course1, requiredCourses.get(0));
@@ -69,7 +66,6 @@ class MajorTester {
         Major major = new Major("Computer Science", null, null);
         Course course1 = new Course("0ac7ca78-5ead-4438-ba85-3e0259eed7a8", "CSCE", "204", "Program Design and Development", null, 0, null, null, null);
         major.addElectiveCourse(course1);
-
         ArrayList<Course> electiveCourses = major.getElectiveCourses();
         assertEquals(1, electiveCourses.size());
         assertEquals(course1, electiveCourses.get(0));
